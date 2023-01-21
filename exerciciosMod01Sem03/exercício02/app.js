f507413cc84510c8f44f9aa0f888a7163c18bae5
carloseduardobc <carlosedubc@gmail.com>
2023-01-21 03:24:49 -0300

    Exercicio 02 - Semana 03

---
+++ exerciciosMod01Sem03/exercício02/app.js
@@ -0,0 +1,9 @@
+
+// const mensagemOla = (mensagem) => {
+const mensagemOla = (nome) => {  
+  // console.log("Olá, Mundo!");
+  nome = "Carlos Eduardo";
+  console.log(`Olá, ${nome}!`);
+}
+
+mensagemOla();
