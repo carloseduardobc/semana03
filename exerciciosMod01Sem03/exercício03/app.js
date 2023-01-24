2808deeb77fbb795eddbef4bc714b3af81693d7d
carloseduardobc <carlosedubc@gmail.com>
2023-01-24 00:30:42 -0300

    Resolução Professor

--- exerciciosMod01Sem03/exercício03/app.js
+++ exerciciosMod01Sem03/exercício03/app.js
@@ -1,7 +1,10 @@
 
-const vetorA = [1, 2, 3, 4, 5];
-const vetorB = [6, 7, 8, 9,10];
+// const vetorA = [1, 2, 3, 4, 5];
+// const vetorB = [6, 7, 8, 9,10];
 
-const novoArray = [...vetorA, ...vetorB];
+// const novoArray = [...vetorA, ...vetorB];
+const concatena = (vetorA, vetorB) => [...vetorA, ...vetorB];
+
+const novoArray = concatena([1, 2, 3, 4, 5], [6, 7, 8, 9,10]);
+
 console.log(novoArray);
-// novo array deve ser [1, 2, 3, 4, 5, 6]
